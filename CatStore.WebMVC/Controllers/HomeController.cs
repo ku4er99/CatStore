7c03143b2129a4a12f52387a04c006f308103e38
Ku4eR99 <Ku4eR99@gmail.com>
2023-03-23 12:40:31 +0300

    Промежуточный коммит для переноса изменений

--- CatStore.WebMVC/Controllers/HomeController.cs
+++ CatStore.WebMVC/Controllers/HomeController.cs
@@ -1,17 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CatStore.WebMVC.Repositories.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CatStore.WebMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPetService _petService;
+
+        public HomeController(IPetService petService)
+        {
+            _petService = petService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var allPets = _petService.GetAll();
+            return View(allPets);
         }
 
         public IActionResult About()
         {
             return View();
+        }
+
+        public IActionResult PetDetail(int petId) {
+            var pet = _petService.GetById(petId);
+            return View(pet);
         }
     }
 }
